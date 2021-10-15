106dcce1b1eb9eb6a132463e44a182e43117e96b
Alexxxx <55647524+AALXX@users.noreply.github.com>
2021-10-15 08:39:14 +0100

    Delete env-config.js

--- config/env-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "process.env.user_id": "user_XxZvZzRWTNt9aS3yTCGLI",
-    "process.env.tamplate_id": "template_7w9ek5n",
-    "process.env.service_id": "service_hp943un",
-};
