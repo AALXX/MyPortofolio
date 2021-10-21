e1adc476549394ad7d39672b01005f654d971d08
AALXX <s3rb@DESKTOP-2D6QHO6.localdomain>
2021-10-21 18:42:12 +0300

    fix about page typos

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className={style.PageContainer}>
             <div className={style.HeadContainer}>
-                <h1 className={style.HeadLine}>Hi, I ‘m a 16 y.o full stack web. As languages I know and use mainly javascript,</h1>
+                <h1 className={style.HeadLine}>Hi, I ‘m a 16 y.o full stack web developer. As languages I know and use mainly javascript,</h1>
                 <h1 className={style.HeadLine}>typescript but i am a bit familiar with c++, python, c#, java</h1>
 
             </div>
