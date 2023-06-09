4a16d0d2b4806f0210266432062c1d9e3be20860
AALXX <s3rb@DESKTOP-2D6QHO6>
2023-06-09 20:14:53 +0300

    backend fix 3 (projects.json not found)

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SERVER_BACKEND: 'https://s3rbvn-portofolio-backend.herokuapp.com/api',
+    SERVER_BACKEND: 'https://portflio-backend.onrender.com/api',
     service_id: 'service_hp943un',
     tamplate_id: 'template_i4kstfa',
     user_id: 'user_XxZvZzRWTNt9aS3yTCGLI'
