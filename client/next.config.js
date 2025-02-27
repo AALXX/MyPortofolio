ee278501ca02b44678fa5cb1c88bc0cf4ff4f481
AALXX <alexn.serban@gmail.com>
2025-02-28 01:28:02 +0200

    first commit

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        service_id: 'service_hp943un',
-        tamplate_id: 'template_i4kstfa',
-        user_id: 'user_XxZvZzRWTNt9aS3yTCGLI'
-    },
-}
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const config = {};
+
+export default config;
--- client/src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function PageNotFound() {
-  return (
-    <div className="flex flex-col justify-center  w-full h-[100vh]">
-      <h1 className="self-center text-white text-lg">Sorry the page you are looking for dosen't exist </h1>
-    </div>
-  );
-}
